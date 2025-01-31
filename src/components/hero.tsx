67f753ba511f53689796f5b0ab30ad6a2d406823
Ojas Singh <ojas.singh02@gmail.com>
2025-01-31 01:55:16 -0500

    Stupid fucking fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="grid h-screen place-content-center items-center pt-14 sm:pt-10">
       <h1 className="font-heading text-center flex gap-2 text-6xl tracking-tight italic">
-        <BlurFade>rene's</BlurFade> <BlurFade delay={0.1}>studio</BlurFade>
+        <BlurFade>rene&apos;s</BlurFade> <BlurFade delay={0.1}>studio</BlurFade>
       </h1>
       <BlurFade delay={0.15}>
         <p className="mt-4 text-sm text-gray-500 uppercase text-center">
